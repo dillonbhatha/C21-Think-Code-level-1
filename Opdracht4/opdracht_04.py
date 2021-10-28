7bbba6abdb0d582dc49982cae201a2fcfeb97555
dillon bhatha <dillon.bhatha@student.rocva.nl>
2021-10-28 10:42:55 +0200

    alle fouten verbeterd

--- Opdracht4/opdracht_04.py
+++ Opdracht4/opdracht_04.py
@@ -1,6 +1,31 @@
-age = 
+age = 13
 
-if conditie:
+if 15 >= age + 2:
+    print(True)
+else:
+    print(False)
+
+if 15 == 3 * 5:
+    print(True)
+else:
+    print(False)
+
+if 15 <= 3 * 5:
+    print(True)
+else:
+    print(False)
+
+if age != 10:
+    print(True)
+else:
+    print(False)
+
+if 17 < 17:
+    print(True)
+else:
+    print(False)
+
+if "Frank" == "Sarah":
     print(True)
 else:
     print(False)
