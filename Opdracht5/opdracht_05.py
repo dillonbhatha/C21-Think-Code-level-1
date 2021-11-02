8d480f8a8c52599767d8db96842de11b5f98ee18
dillon bhatha <dillon.bhatha@student.rocva.nl>
2021-11-02 13:43:44 +0100

    opdracht 5 klaar

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,10 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities\
+
+def favorite_games(name):
+    print("my favorite game is" , name)
+
+favorite_games("overwatch")
+favorite_games("FIFA")
+favorite_games("call of duty")
