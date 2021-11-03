7d734d2773d67f4ffdcad390689f7e868bb31dff
dillonbhatha <90607097+dillonbhatha@users.noreply.github.com>
2021-11-03 14:12:14 +0100

    eindopdracht

---
+++ eindopdracht think code/eindopdracht think code.py
@@ -0,0 +1,27 @@
+#username
+print ('enter your name:')
+name=input()
+print('hello', name)
+print ('welcome')
+#questions
+question =print('De hoofdstad van Nederland is [A] Amsterdam of [B] Utrecht')
+question=input()
+if question ==("A"):
+    print("Goed!")
+elif question ==("B"):
+    print("Jammer.")
+
+question =print('De hoofdstad van Turkije is [A] Ankara of [B] Abu Dhabi')
+question=input()
+if question ==("A"):
+    print("Goed!")
+elif question ==("B"):
+    print("Jammer.")
+
+question =print('De hoofdstad van Oostenrijk is [A] Graz of [B] Vienna')
+question=input()
+if question ==('A'):
+    print("Jammer.")
+elif question ==("B"):
+    print("Goed!")
+print("Bedankt voor het meedoen")
